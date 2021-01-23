dd614221418ac5bb9b12e910b95adb211506c055
jjbc-wefish <juanjo.bernabe@wefish.app>
2021-01-24 00:43:36 +0100

    incluir Jenkinsfile

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Agente de jenkin funcionando!';
+    return 'Agente de Jenkin funcionando!';
   }
 }
